4346641274aaa8a69b8dc382604d0210af40a91c
meysamzamani <meysamzamani@gmail.com>
2023-09-14 19:47:00 +0200

    style: define base style and import basic requirement to handle this project

---
+++ src/main/java/com/meysamzamani/datasynchronizer/DataSynchronizerApplication.java
@@ -0,0 +1,13 @@
+package com.meysamzamani.datasynchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSynchronizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSynchronizerApplication.class, args);
+    }
+
+}
